5a484d924b0a590e9adfd55c790a355c7a15b90d
Prahnu Bordoloi <99647222+prahnu@users.noreply.github.com>
2022-03-21 14:04:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDom from "react-dom";
 
 
-ReactDom.render(<h1>Hello</h1>, documents.getElementsById("root"));
+ReactDom.render(<h1>Hello REact js</h1>, documents.getElementsById("root"));
